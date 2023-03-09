a7bb77a98f5de480ddea610ba6a4ddc5cf9fd69c
EriksOsis <osiseriks2@gmail.com>
2023-03-09 21:27:38 +0200

    Initial commit

---
+++ src/components/DescriptionSection/DescriptionSection.jsx
@@ -0,0 +1,21 @@
+import './DescriptionSection.css';
+
+export function DescriptionSection() {
+    return (
+        <section className={'description-container'}>
+            <div className={'img-container'}>
+                {/*<img/>*/}
+            </div>
+            <div className={'description-text-container'}>
+                <p className={'description-text'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    architecto autem dicta ducimus eligendi explicabo fuga fugit ipsum laborum magni maiores minima
+                    natus odit placeat, quae quis repellat vel velit? Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Accusamus culpa debitis ea eligendi error esse facilis, incidunt itaque magnam
+                    optio quae quibusdam quo quos ratione reiciendis tempore vitae? Error, placeat. Lorem ipsum dolor
+                    sit amet, consectetur adipisicing elit. Architecto asperiores assumenda at culpa debitis, esse
+                    facilis fugiat in, laborum maxime minus nihil nisi numquam quos similique ullam voluptatibus. Dolor,
+                    officia?</p>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/UI/Divider.jsx
@@ -0,0 +1,5 @@
+export function Divider() {
+    return (
+        <hr className={classes.divider}/>
+    )
+}
