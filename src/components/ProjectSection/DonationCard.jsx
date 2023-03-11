9913103102bce235878583e00de945ab5a3cf4b0
HeiwanStudio <colab@heiwan.studio>
2023-03-12 00:28:49 +0200

    update Project
    added animations

--- src/components/ProjectSection/DonationCard.jsx
+++ src/components/ProjectSection/DonationCard.jsx
@@ -1,58 +1,118 @@
 import './DonationCard.css';
 import {useState} from "react";
+import { gsap } from "gsap";
+
 
 export function DonationCard(props) {
     const [containerAnimation, setContainerAnimation] = useState({});
     const [imgAnimation, setImgAnimation] = useState({});
 
-    function onHoverHandler() {
-        setContainerAnimation({
-            animationName: 'container-open',
-            animationDuration: '.2s',
-            animationTimingFunction: 'ease-out',
-            animationFillMode: 'forwards'
-        });
+    // const [gsapTL, setGsapTL] = useState(gsap.timeline());
 
-        setImgAnimation({
-            animationName: 'img-open',
-            animationDuration: '.2s',
-            animationTimingFunction: 'ease-out',
-            animationFillMode: 'forwards',
-            animationDelay: '.5s'
-        });
-    }
+    const tl = gsap.timeline({paused: false})
 
-    function offHoverHandler() {
-        setContainerAnimation({
-            animationName: 'container-close',
-            animationDuration: '.2s',
-            animationTimingFunction: 'ease-out',
-            animationFillMode: 'forwards',
-            animationDelay: '.5s'
-        });
+    let init = false
 
-        setImgAnimation({
-            animationName: 'img-close',
-            animationDuration: '.2s',
-            animationTimingFunction: 'ease-out',
-            animationFillMode: 'forwards',
-            animationDelay: '0s'
-        });
-    }
+    // const parent = this
+    // const bgBox = parent.querySelector(".donation__bg")
+    // const imgBox = parent.querySelector(".donation__image")
+    // const title = parent.querySelector("h2")
+
+    // tl.to(bgBox, { width: '100%', duration: 0.1 })
+    // tl.to(title, { backgroundPositionX: '0%', duration: 0.2 })
+    // tl.to(imgBox, { translateY: "0%" })
+
+    // tl.revese()
+
+    const onEnter = ({ currentTarget }) => {
+        const parent = currentTarget
+        const bgBox = parent.querySelector(".donation__bg")
+        const imgBox = parent.querySelector(".donation__image")
+        const title = parent.querySelector("h2")
+        if (!init) {
+            tl.to(bgBox, { width: '100%', duration: 0.1 })
+            tl.to(title, { backgroundPositionX: '0%', duration: 0.2 })
+            tl.to(imgBox, { translateY: "0%", duration: 0.3 })
+            init = true
+        }
+
+        tl.reversed(false)
+
+        console.log(bgBox);
+
+        
+    };
+
+    const onLeave = ({ currentTarget }) => {
+        tl.reversed(!tl.reversed())
+        console.log("stuff");
+        // gsap.to(currentTarget, { backgroundColor: "#28a92b", scale: 1 });
+    };
+
+    // function onHoverHandler() {
+    //     setContainerAnimation({
+    //         animationName: 'container-open',
+    //         animationDuration: '.2s',
+    //         animationTimingFunction: 'ease-out',
+    //         animationFillMode: 'forwards'
+    //     });
+
+    //     setImgAnimation({
+    //         animationName: 'img-open',
+    //         animationDuration: '.2s',
+    //         animationTimingFunction: 'ease-out',
+    //         animationFillMode: 'forwards',
+    //         animationDelay: '.5s'
+    //     });
+    // }
+
+    // function offHoverHandler() {
+    //     setContainerAnimation({
+    //         animationName: 'container-close',
+    //         animationDuration: '.2s',
+    //         animationTimingFunction: 'ease-out',
+    //         animationFillMode: 'forwards',
+    //         animationDelay: '.5s'
+    //     });
+
+    //     setImgAnimation({
+    //         animationName: 'img-close',
+    //         animationDuration: '.2s',
+    //         animationTimingFunction: 'ease-out',
+    //         animationFillMode: 'forwards',
+    //         animationDelay: '0s'
+    //     });
+    // }
 
     return (
-        <div className={'donation-container'}>
-            <div className={'donation-img-container'}>
-                <img className={'donation-img'} src={props.img} alt={`${props.heading} image`} style={imgAnimation}/>
-            </div>
-            <div className={'overlay'}>
-                <div className={'heading-container-open'} style={containerAnimation}/>
-            </div>
-            <div className={'heading-container'}>
-                <h2 className={'donation-heading'} onMouseEnter={onHoverHandler} onMouseLeave={offHoverHandler}>
+        <div className={'donation'} onMouseEnter={onEnter} onMouseLeave={onLeave}>
+
+            <div className="donation__title" >
+                <div className="donation__bg"></div>
+                <h2>
                     {props.heading}
                 </h2>
             </div>
+
+            <div className="donation__image"
+                style={{
+                    backgroundImage: `url(${(props.img)})`
+                }}>
+            </div>
+
+            {
+                /* <div className={'donation-img-container'}>
+                    <img className={'donation-img'} src={props.img} alt={`${props.heading} image`} style={imgAnimation}/>
+                </div>
+                <div className={'overlay'}>
+                    <div className={'heading-container-open'} style={containerAnimation}/>
+                </div>
+                <div className={'heading-container'} onMouseEnter={onEnter} onMouseLeave={onLeave}>
+                    <h2 className={'donation-heading'}>
+                        {props.heading}
+                    </h2>
+                </div> */
+            }
         </div>
     )
 }
