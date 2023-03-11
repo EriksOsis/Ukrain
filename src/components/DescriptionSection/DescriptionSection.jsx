0422a5399a44e99d64a6458a91a7aa575a425aed
EriksOsis <osiseriks2@gmail.com>
2023-03-11 12:55:58 +0200

    Fixed styling for experience section

--- src/components/DescriptionSection/DescriptionSection.jsx
+++ src/components/DescriptionSection/DescriptionSection.jsx
@@ -9,17 +9,20 @@ export function DescriptionSection() {
             </div>
             <div className={'description-text-container'}>
                 <p className={'description-text'}>
-                    Over the past year, we have gotten used to news about the brutal invasion of Russia in Ukraine and
-                    its
-                    current situation. However, the war is still very active, and the future of Ukraine and all of free
-                    Europe
-                    is at stake. When Ukraine wins, we'll sigh in relief, but for all of us to achieve that there is
-                    need for all
-                    of us to come together and help by any ways we can, either it will be monetary help or by taking
-                    active involvement in all. <br/>
-                    By collaborating of people from Latvia and Ukraine and invoking embassy and politicians from both
-                    countries, we have achieved government support donation program, which will help to relief the war
-                    efforts and to rebuild Ukraine after the war. <br/>
+                    Over the past year, we have gotten used to news about the brutal inva-<br/>
+                    sion of Russia in Ukraine and its<br/>
+                    current situation. However, the war is still very active, and the<br/>
+                    future of Ukraine and all of free Europe <br/>
+                    is at stake. When Ukraine wins, we'll sigh in relief, but for all of<br/>
+                    us to achieve that there is need for all<br/>
+                    of us to come together and help by any ways we can, either it will be<br/>
+                    monetary help or by taking <br/>
+                    active involvement in all.<br/>
+                    By collaborating of people from Latvia and Ukraine and invoking embas-<br/>
+                    sy and politicians from both<br/>
+                    countries, we have achieved government support donation program, which<br/>
+                    will help to relief the war<br/>
+                    efforts and to rebuild Ukraine after the war.
                 </p>
             </div>
         </section>
